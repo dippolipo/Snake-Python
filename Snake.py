2c720030dcd61c424c17c3b8131c8e124753611e
dippolipo <gioele.dippolito@gmail.com>
2025-12-21 12:33:35 +0100

    Better readability, added not implemented pause variable

--- Snake.py
+++ Snake.py
@@ -1,14 +1,14 @@
-import pygame
+import pygame as pg
 from pygame import Vector2
 
 import random
 
 def init():
-    pygame.init()
-    if not pygame.display.get_init():
+    pg.init()
+    if not pg.display.get_init():
         print("display non-initiated")
         input()
-        pygame.quit()
+        pg.quit()
 
 
 init()
@@ -17,21 +17,21 @@ init()
 def load_tileset(tileset_image, tile_size):
 
     tileset_size = (int(tileset_image.get_width() / tile_size), int(tileset_image.get_height() / tile_size))
-    single_tiles = [pygame.Surface((tile_size, tile_size)).convert_alpha()]
-    single_tiles[0].fill(pygame.Color(255, 255, 255, 0))
+    single_tiles = [pg.Surface((tile_size, tile_size)).convert_alpha()]
+    single_tiles[0].fill(pg.Color(255, 255, 255, 0))
 
     for i in range(tileset_size[1]):
         for j in range(tileset_size[0]):
-            single_tiles.append(tileset_image.subsurface(pygame.Rect(j * tile_size, i * tile_size, tile_size, tile_size)))
+            single_tiles.append(tileset_image.subsurface(pg.Rect(j * tile_size, i * tile_size, tile_size, tile_size)))
 
     return single_tiles
 
 
-def draw_tilemap(single_tiles, grid, background_color=pygame.Color(255, 255, 255, 0)):
+def draw_tilemap(single_tiles, grid, background_color=pg.Color(255, 255, 255, 0)):
 
     tile_size = single_tiles[0].get_width()
     grid_size = (len(grid[0]), len(grid))
-    final_canvas = pygame.Surface((grid_size[0] * tile_size, grid_size[1] * tile_size)).convert_alpha()
+    final_canvas = pg.Surface((grid_size[0] * tile_size, grid_size[1] * tile_size)).convert_alpha()
     final_canvas.fill(background_color)
 
     for i, row in enumerate(grid):
@@ -60,25 +60,25 @@ def load_tilemap(file_path):  # file_path needs to be raw
 
 
 running = True
-clock = pygame.time.Clock()
-pygame.display.set_icon(pygame.image.load(r"data/IconSmall.png"))
-pygame.display.set_caption("Snake")
-screen = pygame.display.set_mode((384, 216), pygame.SCALED) # SCALED called to allow fullscreen
+clock = pg.time.Clock()
+pg.display.set_icon(pg.image.load(r"data/IconSmall.png"))
+pg.display.set_caption("Snake")
+screen = pg.display.set_mode((384, 216), pg.SCALED) # SCALED called to allow fullscreen
 TILESIZE = 16
-tileset = load_tileset(pygame.image.load(r"data/Tiles.png").convert_alpha(), TILESIZE)
+tileset = load_tileset(pg.image.load(r"data/Tiles.png").convert_alpha(), TILESIZE)
 max_fps = 30
-pygame.display.toggle_fullscreen()
+pg.display.toggle_fullscreen()
 
 # GAME
 # keys
-up, down, left, right = pygame.K_w, pygame.K_s, pygame.K_a, pygame.K_d
+UP, DOWN, LEFT, RIGHT = pg.K_w, pg.K_s, pg.K_a, pg.K_d
 # constants
 APPLE = 255
+MAPWIDTH = 18
+MAPHEIGHT = 12
 # map
-map_width = 18
-map_height = 12
 initial_snake_length = 3
-game_map= [[0 for i in range(map_width)] for j in range(map_height)]
+game_map= [[0 for i in range(MAPWIDTH)] for j in range(MAPHEIGHT)]
 game_map_render = [row.copy() for row in game_map]
 for x in range(initial_snake_length):
     game_map[5][x + 1] = x+1
@@ -88,14 +88,14 @@ snake_max_speed = 90/max_fps
 snake_speed = 0
 snake_pos = Vector2((initial_snake_length), 5)
 snake_dir = [Vector2(1, 0), Vector2(1, 0)] # 0 = last, 1 = current, ... FIFO
-snake_spritesheet = pygame.image.load(r"./data/Snake.png").convert_alpha()
+snake_spritesheet = pg.image.load(r"./data/Snake.png").convert_alpha()
 current_score = 0
-snake_head_sprite = [snake_spritesheet.subsurface(pygame.Rect(0, 0, TILESIZE, TILESIZE))]
+snake_head_sprite = [snake_spritesheet.subsurface(pg.Rect(0, 0, TILESIZE, TILESIZE))]
 for n in range(3):
-    snake_head_sprite.append(pygame.transform.rotate(snake_head_sprite[n], 90))
-snake_tail_sprite = [snake_spritesheet.subsurface(pygame.Rect(TILESIZE, 0, TILESIZE, TILESIZE))]
+    snake_head_sprite.append(pg.transform.rotate(snake_head_sprite[n], 90))
+snake_tail_sprite = [snake_spritesheet.subsurface(pg.Rect(TILESIZE, 0, TILESIZE, TILESIZE))]
 for n in range(3):
-    snake_tail_sprite.append(pygame.transform.rotate(snake_tail_sprite[n], 90))
+    snake_tail_sprite.append(pg.transform.rotate(snake_tail_sprite[n], 90))
 # init
 background = draw_tilemap(tileset, load_tilemap(r"./data/Map.txt"))
 
@@ -109,7 +109,7 @@ def render_frame():
                 game_map_render[y][x] = 9
             elif cell != 0 and game_map[y][x] == 0:
                 game_map_render[y][x] = 0
-            elif game_map[y][x] <= map_width*map_height and game_map[y][x] != 0:
+            elif game_map[y][x] <= MAPWIDTH*MAPHEIGHT and game_map[y][x] != 0:
                 game_map_render[y][x] = 10
 
     snake_body = draw_tilemap(tileset, game_map_render)
@@ -126,7 +126,7 @@ def render_frame():
 
     screen.blit(snake_body, (48, 12))
 
-    pygame.display.flip()
+    pg.display.flip()
 
 
 def random_cell_replace(grid, starting_value, final_value):
@@ -143,24 +143,27 @@ def random_cell_replace(grid, starting_value, final_value):
 delta = 0
 while running:
     # poll for events
-    for event in pygame.event.get():
+    for event in pg.event.get():
         match event.type:
-            case pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    running = False
+            case pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    pause = True
 
-                if snake_speed == 0 and (event.key == up or event.key == down or event.key == right):
+                if event.key == pg.K_ESCAPE:
+                    pause = True
+
+                if snake_speed == 0 and (event.key == UP or event.key == DOWN or event.key == RIGHT):
                     snake_speed = snake_max_speed
 
-                if event.key == up and not snake_dir[-1] == Vector2(0, 1):
+                if event.key == UP and not snake_dir[-1] == Vector2(0, 1):
                     snake_dir.append(Vector2(0, -1))
-                elif event.key == down and not snake_dir[-1] == Vector2(0, -1):
+                elif event.key == DOWN and not snake_dir[-1] == Vector2(0, -1):
                     snake_dir.append(Vector2(0, 1))
-                elif event.key == left and not snake_dir[-1] == Vector2(1, 0):
+                elif event.key == LEFT and not snake_dir[-1] == Vector2(1, 0):
                     snake_dir.append(Vector2(-1, 0))
-                elif event.key == right and not snake_dir[-1] == Vector2(-1, 0):
+                elif event.key == RIGHT and not snake_dir[-1] == Vector2(-1, 0):
                     snake_dir.append(Vector2(1, 0))
-            case pygame.QUIT:
+            case pg.QUIT:
                 running = False
 
     # movement
@@ -171,7 +174,7 @@ while running:
         snake_pos += snake_dir[1]
         snake_posx = int(snake_pos.x)
         snake_posy = int(snake_pos.y)
-        if snake_posx < 0 or snake_posx >= map_width or snake_posy < 0 or snake_posy >= map_height or (game_map[snake_posy][snake_posx] != 0 and game_map[snake_posy][snake_posx] < map_width * map_height):
+        if snake_posx < 0 or snake_posx >= MAPWIDTH or snake_posy < 0 or snake_posy >= MAPHEIGHT or (game_map[snake_posy][snake_posx] != 0 and game_map[snake_posy][snake_posx] < MAPWIDTH * MAPHEIGHT):
             print("STOP") # change room
             snake_speed = 0
         else:
@@ -181,7 +184,7 @@ while running:
             else:
                 for y in range(len(game_map)):
                     for x in range(len(game_map[0])):
-                        if game_map[y][x] != 0 and game_map[y][x] < map_width * map_height:
+                        if game_map[y][x] != 0 and game_map[y][x] < MAPWIDTH * MAPHEIGHT:
                             game_map[y][x] -= 1
             game_map[snake_posy][snake_posx] = current_score + initial_snake_length
         snake_dir.pop(0)
@@ -191,4 +194,4 @@ while running:
     render_frame()
     clock.tick(max_fps)
 
-pygame.quit()
+pg.quit()
