b8b95baff68f4b7d398344e5cc7f1c87db3fa4f7
dippolipo <gioele.dippolito@gmail.com>
2025-12-26 01:15:27 +0100

    Updated graphics

--- scripts/globs.py
+++ scripts/globs.py
@@ -4,7 +4,7 @@ import pygame as pg
 engine.pygame_init("Snake", r"data/IconSmall.png", (384, 216))
 
 TILE_SIZE = 16
-tileset = engine.load_tileset(pg.image.load(r"data/Tiles.png").convert_alpha(), TILE_SIZE)
+tileset = engine.load_tileset(pg.image.load(r"data/Tilemap.png").convert_alpha(), TILE_SIZE)
 entities_tiles = engine.load_tileset(pg.image.load(r"data/Entities.png").convert_alpha(), TILE_SIZE)
 font =  engine.FontPNG(r"data/Font")
 UP, DOWN, LEFT, RIGHT, A, B, PAUSE= pg.K_w, pg.K_s, pg.K_a, pg.K_d, pg.K_j, pg.K_i, pg.K_ESCAPE
